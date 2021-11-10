59d1dc40ff1558f2b5c24478a7e90b7b25adf35d
guilhermem0101 <guilherme.sou@hotmail.com>
2021-11-10 11:38:11 -0300

    post&get

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -8,14 +10,33 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./tema.component.css']
 })
 export class TemaComponent implements OnInit {
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private temaService: TemaService) { }
 
   ngOnInit() {
     if(environment.token ==''){
       //alert('sua sessão expirou.')
       this.router.navigate(['/entrar'])
     }
+
+    this.findAllTemas()
   }
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[])=> {
+      this.listaTemas = resp
+    })
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+      this.tema = resp
+      alert ('tema cadastrado com sucesso')
+      this.findAllTemas()
+      this.tema = new Tema()
+    })
+  }
+
 
 }
